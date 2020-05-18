a327036ed36b16ccf1d1d3e2f6816f23a3f55e99
WeiPei2219062260 <2219062260@qq.com>
2020-05-18 16:52:08 +0800

    实现查询

---
+++ MvcDemo/MvcDemo/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using MvcDemo.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private GoodsDBEntities1 entities = new GoodsDBEntities1();
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            var goods = entities.Goods.ToList();
+            ViewBag.goods = goods;
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var goods = entities.Goods.FirstOrDefault(x => x.GoodsID == id);
+            entities.Goods.Remove(goods);
+            entities.SaveChanges();
+            return Content("OK");
+        }
+    }
+}
